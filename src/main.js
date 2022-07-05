cb36d047c07e9b6725449a39e38cbea52a7dfca8
EstherChen000 <estherchen000@gmail.com>
2022-07-05 21:26:12 +0800

    引入地圖API

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
+import * as VueGoogleMaps from 'vue2-google-maps';
 
 import App from './App.vue';
 import router from './router';
@@ -11,6 +12,13 @@ import vuetify from './plugins/vuetify';
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
 Vue.component('Loading', Loading);
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_MAP,
+    libraries: 'places',
+  },
+  installComponents: true,
+});
 
 new Vue({
   router,
--- src/views/pages/ScenePage.vue
+++ src/views/pages/ScenePage.vue
@@ -3,68 +3,99 @@
     <loading :active.sync="isLoading"></loading>
     <Navbar></Navbar>
     <v-row class="d-flex flex-no-wrap justify-space-between w-100 mt-6">
-      <v-col cols="12" sm="6">
+      <v-col cols="12"
+             sm="6">
         <h2 class="font-weight-bold pt-0 pb-8 text-h3">
-          {{scenic[0].ScenicSpotName || scenic[0].RestaurantName}}
+          {{ scenic[0].ScenicSpotName || scenic[0].RestaurantName }}
         </h2>
-        <p class="py-0 text-h4 grey--text">{{scenic[0].Class1 || '其他'}}</p>
+        <p class="py-0 text-h4 grey--text">{{ scenic[0].Class1 || "其他" }}</p>
         <!-- vp > 600px 顯示 呈現文左圖右 -->
         <div class="d-none d-sm-flex flex-column">
           <p class="py-0 text-body-1">
-            <i class="fas fa-phone-alt grey--text mr-3"
-            ></i>{{scenic[0].Phone}}<span class="primary--text ml-3">撥打電話</span></p>
+            <i class="fas fa-phone-alt grey--text mr-3"></i>{{ scenic[0].Phone
+            }}<span class="primary--text ml-3">撥打電話</span>
+          </p>
           <p class="py-0 text-body-1">
-            <i class="fas fa-clock grey--text mr-3"
-            ></i>{{scenic[0].OpenTime}}</p>
+            <i class="fas fa-clock grey--text mr-3"></i>{{ scenic[0].OpenTime }}
+          </p>
           <p class="py-0 text-body-1">
-            <i class="fas fa-map-marker-alt mr-1 grey--text mr-3"
-            ></i>{{scenic[0].Address}}</p>
+            <i class="fas fa-map-marker-alt mr-1 grey--text mr-3"></i>{{ scenic[0].Address }}
+          </p>
         </div>
       </v-col>
       <!-- vp > 600px 不顯示, 當<600px時 呈現標題-圖-資訊 -->
-      <v-col cols="12" sm="6" class="d-flex d-sm-none">
-        <v-img class="rounded-lg" height="300px"
-        :src="scenic[0].Picture.PictureUrl1 || scenic[0].Picture.PictureUrl2
-        || availableImg"></v-img>
+      <v-col cols="12"
+             sm="6"
+             class="d-flex d-sm-none">
+        <v-img class="rounded-lg"
+               height="300px"
+               :src="scenic[0].Picture.PictureUrl1 ||
+               scenic[0].Picture.PictureUrl2 || availableImg">
+        </v-img>
       </v-col>
-      <v-col cols="12" sm="6" class="d-flex d-sm-none flex-column">
+      <v-col cols="12"
+             sm="6"
+             class="d-flex d-sm-none flex-column">
         <p class="py-0 text-body-1">
-          <i class="fas fa-phone-alt grey--text mr-3"
-          ></i>{{scenic[0].Phone}}<span class="primary--text ml-3">撥打電話</span></p>
+          <i class="fas fa-phone-alt grey--text mr-3"></i>{{ scenic[0].Phone
+          }}<span class="primary--text ml-3">撥打電話</span>
+        </p>
         <p class="py-0 text-body-1">
-          <i class="fas fa-clock grey--text mr-3"
-          ></i>{{scenic[0].OpenTime}}</p>
+          <i class="fas fa-clock grey--text mr-3"></i>{{ scenic[0].OpenTime }}
+        </p>
         <p class="py-0 text-body-1">
-          <i class="fas fa-map-marker-alt mr-1 grey--text mr-3"
-          ></i>{{scenic[0].Address}}</p>
+          <i class="fas fa-map-marker-alt mr-1 grey--text mr-3"></i>{{ scenic[0].Address }}
+        </p>
       </v-col>
       <!-- vp > 600px 顯示 呈現文左圖右 -->
-      <v-col cols="12" sm="6" class="d-none d-sm-flex">
-        <v-img class="rounded-lg" height="300px"
-        :src="scenic[0].Picture.PictureUrl1 || scenic[0].Picture.PictureUrl2
-        || availableImg"></v-img>
+      <v-col cols="12"
+             sm="6"
+             class="d-none d-sm-flex">
+        <v-img class="rounded-lg"
+               height="300px"
+               :src="scenic[0].Picture.PictureUrl1 ||
+               scenic[0].Picture.PictureUrl2 || availableImg">
+        </v-img>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="12">
         <h3 class="text-h4 font-weight-bold mb-6">景點介紹</h3>
         <p>
-          {{scenic[0].DescriptionDetail || scenic[0].Description}}
+          {{ scenic[0].DescriptionDetail || scenic[0].Description }}
         </p>
       </v-col>
     </v-row>
-    <v-row class="mt-16 mb-13">
-      <v-col cols="12" sm="12">
-        <h3 class="text-h4 font-weight-bold">
-          附近餐飲
-        </h3>
+    <v-row>
+      <v-col cols="12">
+        <h3 class="text-h4 font-weight-bold mb-6">地圖</h3>
       </v-col>
-      <v-col cols="12" sm="4" v-for="item in getRestaurantThree" :key="item.ID">
+      <v-col cols="12">
+        <GmapMap :center="center"
+                 :zoom="15"
+                 :options="options"
+                 style="width: 100%; height: 600px">
+          <GmapMarker :position="marker.position"
+                      :clickable="true"
+                      :draggable="false" />
+        </GmapMap>
+      </v-col>
+    </v-row>
+    <v-row class="mt-16 mb-13">
+      <v-col cols="12"
+             sm="12">
+        <h3 class="text-h4 font-weight-bold">附近餐飲</h3>
+      </v-col>
+      <v-col cols="12"
+             sm="4"
+             v-for="item in getRestaurantThree"
+             :key="item.ID">
         <v-card @click="getRestaurants(item.RestaurantID)"
-        class="rounded-lg" outlined height="400px">
-          <v-img :src="item.Picture.PictureUrl1 || item.Picture.PictureUrl2
-          || availableImg"
-          height="250px"></v-img>
+                class="rounded-lg"
+                outlined
+                height="400px">
+          <v-img :src="item.Picture.PictureUrl1 || item.Picture.PictureUrl2 || availableImg"
+                 height="250px"></v-img>
           <v-card-text class="primary--text py-2">餐飲</v-card-text>
           <v-card-title class="font-weight-bold pt-0 pb-8 text-h5">
             {{ item.RestaurantName }}
@@ -97,6 +128,17 @@ export default {
     selectedCity: '',
     availableImg: 'https://picsum.photos/200/200/?random=4',
     isLoading: false,
+    marker: { position: { lat: null, lng: null } },
+    center: { lat: null, lng: null },
+    options: {
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      fullscreenControl: false,
+      disableDefaultUI: false,
+      scrollwheel: true,
+      clickableIcons: true,
+    },
   }),
   methods: {
     getScenicSpot() {
@@ -107,46 +149,74 @@ export default {
       const code2 = /C3_\w+/;
       vm.isLoading = true;
       if (vm.id.match(code1)) {
-        vm.$http.get(api1, { headers: vm.getAuthorizationHeader() }).then((response) => {
-          vm.scenic = response.data;
-          vm.selectedCity = response.data[0].City;
-          vm.getRestaurant();
-        });
+        vm.$http
+          .get(api1, { headers: vm.getAuthorizationHeader() })
+          .then((response) => {
+            vm.scenic = response.data;
+            vm.marker = {
+              position: {
+                lat: vm.scenic[0].Position.PositionLat,
+                lng: vm.scenic[0].Position.PositionLon,
+              },
+            };
+            vm.center = {
+              lat: vm.scenic[0].Position.PositionLat,
+              lng: vm.scenic[0].Position.PositionLon,
+            };
+            vm.location = {
+              lat: vm.scenic[0].Position.PositionLat,
+              lng: vm.scenic[0].Position.PositionLon,
+            };
+            vm.selectedCity = response.data[0].City;
+            vm.getRestaurant();
+          });
       } else if (vm.id.match(code2)) {
-        vm.$http.get(api2, { headers: vm.getAuthorizationHeader() }).then((response) => {
-          vm.scenic = response.data;
-          vm.selectedCity = response.data[0].City;
-          vm.getRestaurant();
-        });
+        vm.$http
+          .get(api2, { headers: vm.getAuthorizationHeader() })
+          .then((response) => {
+            vm.scenic = response.data;
+            vm.location = {
+              lat: response.data.Position.PositionLat,
+              lng: response.data.Position.PositionLon,
+            };
+            vm.selectedCity = response.data[0].City;
+            vm.getRestaurant();
+          });
       }
     },
     getRestaurant() {
       const vm = this;
       const api = `https://ptx.transportdata.tw/MOTC/v2/Tourism/Restaurant${vm.cityTranslate}?$format=JSON`;
-      const regex = /[0-9]/ig;
-      const addressSpot = vm.scenic[0].Address.replace(vm.selectedCity, '').replace(regex, '').slice(0, 3);
-      vm.$http.get(api, { headers: vm.getAuthorizationHeader() }).then((response) => {
-        response.data.forEach((e) => {
-          if (e.Address.includes(addressSpot)) {
-            vm.restaurant.push(e);
-          } else {
-            vm.restaurant.push(e);
-          }
+      const regex = /[0-9]/gi;
+      const addressSpot = vm.scenic[0].Address.replace(vm.selectedCity, '')
+        .replace(regex, '')
+        .slice(0, 3);
+      vm.$http
+        .get(api, { headers: vm.getAuthorizationHeader() })
+        .then((response) => {
+          response.data.forEach((e) => {
+            if (e.Address.includes(addressSpot)) {
+              vm.restaurant.push(e);
+            } else {
+              vm.restaurant.push(e);
+            }
+          });
+          vm.isLoading = false;
         });
-        vm.isLoading = false;
-      });
     },
     getRestaurants(id) {
       const api = `https://ptx.transportdata.tw/MOTC/v2/Tourism/Restaurant?$filter=RestaurantID%20eq%20'${id}'&$format=JSON`;
       const vm = this;
-      vm.$http.get(api, { headers: vm.getAuthorizationHeader() }).then((response) => {
-        vm.scenic = response.data;
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth',
+      vm.$http
+        .get(api, { headers: vm.getAuthorizationHeader() })
+        .then((response) => {
+          vm.scenic = response.data;
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          });
+          vm.$router.push(`/scenePage/${id}`).catch(() => {});
         });
-        vm.$router.push(`/scenePage/${id}`).catch(() => {});
-      });
     },
     getAuthorizationHeader() {
       const appID = '4f650471c3a946078042df58395fa922';
@@ -156,15 +226,64 @@ export default {
       ShaObj.setHMACKey(appKey, 'TEXT');
       ShaObj.update(`x-date: ${GMTString}`);
       const HMAC = ShaObj.getHMAC('B64');
-      const authorization = 'hmac username="' + appID + '", algorithm="hmac-sha1", headers="x-date", signature="' + HMAC + '"';
+      const authorization = 'hmac username="'
+      + appID
+      + '", algorithm="hmac-sha1", headers="x-date", signature="'
+      + HMAC + '"';
       return { Authorization: authorization, 'x-date': GMTString };
     },
   },
   computed: {
     cityTranslate() {
       const vm = this;
-      const city = ['臺北市', '新北市', '桃園市', '臺中市', '臺南市', '高雄市', '基隆市', '新竹市', '新竹縣', '苗栗縣', '彰化縣', '南投縣', '雲林縣', '嘉義縣', '嘉義市', '屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '金門縣', '澎湖縣', '連江縣'];
-      const cityEn = ['Taipei', 'NewTaipei', 'Taoyuan', 'Taichung', 'Tainan', 'Kaohsiung', 'Keelung', 'Hsinchu', 'HsinchuCounty', 'MiaoliCounty', 'ChanghuaCounty', 'NantouCounty', 'YunlinCounty', 'ChiayiCounty', 'Chiayi', 'PingtungCounty', 'YilanCounty', 'HualienCounty', 'TaitungCounty', 'KinmenCounty', 'PenghuCounty', 'LienchiangCounty'];
+      const city = [
+        '臺北市',
+        '新北市',
+        '桃園市',
+        '臺中市',
+        '臺南市',
+        '高雄市',
+        '基隆市',
+        '新竹市',
+        '新竹縣',
+        '苗栗縣',
+        '彰化縣',
+        '南投縣',
+        '雲林縣',
+        '嘉義縣',
+        '嘉義市',
+        '屏東縣',
+        '宜蘭縣',
+        '花蓮縣',
+        '臺東縣',
+        '金門縣',
+        '澎湖縣',
+        '連江縣',
+      ];
+      const cityEn = [
+        'Taipei',
+        'NewTaipei',
+        'Taoyuan',
+        'Taichung',
+        'Tainan',
+        'Kaohsiung',
+        'Keelung',
+        'Hsinchu',
+        'HsinchuCounty',
+        'MiaoliCounty',
+        'ChanghuaCounty',
+        'NantouCounty',
+        'YunlinCounty',
+        'ChiayiCounty',
+        'Chiayi',
+        'PingtungCounty',
+        'YilanCounty',
+        'HualienCounty',
+        'TaitungCounty',
+        'KinmenCounty',
+        'PenghuCounty',
+        'LienchiangCounty',
+      ];
       const a = city.indexOf(vm.selectedCity);
       if (vm.selectedCity === '') {
         return '/';
